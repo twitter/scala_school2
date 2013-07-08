2d6613a7a9abc9c5d37881d22114408d48659f4c
Gary Morrison <gmorrison@twitter.com>
2013-07-08 15:57:25 -0700

    Updated Example test to WordSpec with MustMatchers testing style

--- src/test/scala/ExampleTest.scala
+++ src/test/scala/ExampleTest.scala
@@ -1,6 +1,6 @@
 import org.scalatest.FunSpec
 
-class ExampleTest extends FunSpec {
+class ExampleTest extends WordSpec with MustMatchers {
   
   describe("One Test") {
     it("should pass an assert") {
