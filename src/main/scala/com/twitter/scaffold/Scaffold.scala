409a7aa3285306fe830865f6cfce41c8d8373dab
Eugene Ma <ema@twitter.com>
2013-07-08 16:24:28 -0700

    implement console map

--- src/main/scala/com/twitter/scaffold/Scaffold.scala
+++ src/main/scala/com/twitter/scaffold/Scaffold.scala
@@ -2,14 +2,14 @@ package com.twitter.scaffold
 
 import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
 import akka.io.IO
+import scala.collection.mutable
+import scala.util.Random
 import spray.can.Http
 import spray.http.StatusCodes.{ BadRequest, NoContent }
 import spray.routing.HttpService
 
 class Scaffold extends Actor with HttpService {
-
-  // TODO #6: there should be many console actors supervised by Scaffold, one per user session
-  val console = context.actorOf(Console.props, "console")
+  import Scaffold.ConsoleId
 
   /* HttpService */
   override val actorRefFactory = context
@@ -38,27 +38,51 @@ class Scaffold extends Actor with HttpService {
       }
     }
 
+  private[this] val consoles = mutable.Map.empty[ConsoleId, ActorRef]
+  private[this] val random = new Random()
+
   private[this] val consoleRoute =
-    post {
-      import com.twitter.spray._
-      entity(as[String]) {
-        Console.Interpret(_) ~> console ~> {
-          case Console.Success(message) => complete { message }
-          case Console.Failure(message) => respondWithStatus(BadRequest) { complete { message } }
+    path("console") {
+      post {
+        complete {
+          val id = random.nextInt().abs
+          val console = context.actorOf(Console.props, "console-%d".format(id))
+          consoles(id) = console
+          id.toString
         }
       }
     } ~
-    delete {
-      complete {
-        console ! Console.Reset
-        NoContent
+    path("console" / LongNumber) { id =>
+      post {
+        import com.twitter.spray._
+        consoles.get(id) match {
+          case Some(console) =>
+            entity(as[String]) {
+              Console.Interpret(_) ~> console ~> {
+                case Console.Success(message) => complete { message }
+                case Console.Failure(message) => respondWithStatus(BadRequest) { complete { message } }
+              }
+            }
+          case None => reject
+        }
+      } ~
+      delete {
+        consoles.get(id) match {
+          case Some(console) =>
+            complete {
+              console ! Console.Reset
+              NoContent
+            }
+          case None => reject
+        }
       }
     }
-
 }
 
 object Scaffold extends App {
   implicit val system = akka.actor.ActorSystem("scaffold-system")
+
+  type ConsoleId = Long
 
   val props = Props[Scaffold]
   val scaffold = system.actorOf(props, "scaffold")
